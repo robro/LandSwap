4db46b9970d295af7d76dd6fdc6c56ecb63c52c3
robro <pibborando@gmail.com>
2019-03-23 09:28:58 -0700

    :wrench: Stop flooding the namespace

--- landswap.py
+++ landswap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import random
 import pyperclip as pyclip
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
 from PIL import ImageTk, Image
@@ -31,10 +31,10 @@ class ModifiedText(ScrolledText):
 		return result
 
 
-class LandFrame(Frame):
+class LandFrame(tk.Frame):
 
 	def __init__(self, master, land_type):
-		Frame.__init__(self, master)
+		tk.Frame.__init__(self, master)
 		self.master = master
 		self.land_type = land_type
 
@@ -49,63 +49,63 @@ class LandFrame(Frame):
 		self.land_list = list(self.images.keys())
 		self.image_container = ttk.Label(self)
 
-		self.land_var = StringVar(self)
+		self.land_var = tk.StringVar(self)
 		self.land_var.trace('w', lambda *_: master.on_land_change(self.land_var, land_type))
 		self.land_var.set(random.choice(list(LANDS[land_type])))
 
 		self.image_container.config(image=self.images[self.land_var.get()])
-		self.image_container.grid(row=0, column=0, sticky=S, padx=10, pady=10)
+		self.image_container.grid(row=0, column=0, sticky='s', padx=10, pady=10)
 
 		self.land_dropdown = ttk.Combobox(self, width=20, textvariable=self.land_var, values=self.land_list, state='readonly')
-		self.land_dropdown.grid(row=1, column=0, sticky=W, padx=15, pady=10)
+		self.land_dropdown.grid(row=1, column=0, sticky='w', padx=15, pady=10)
 
 		self.prev_button = ttk.Button(self, text='Prev', width=6, command=lambda: master.set_to_prev(self.land_list, self.land_var, land_type))
-		self.prev_button.grid(row=1, column=0, sticky=E, padx=65)
+		self.prev_button.grid(row=1, column=0, sticky='e', padx=65)
 
 		self.next_button = ttk.Button(self, text='Next', width=6, command=lambda: master.set_to_next(self.land_list, self.land_var, land_type))
-		self.next_button.grid(row=1, column=0, sticky=E, padx=15)
+		self.next_button.grid(row=1, column=0, sticky='e', padx=15)
 
 
 	def enable(self):
-		self.image_container.config(state=NORMAL)
+		self.image_container.config(state='normal')
 		self.land_dropdown.config(state='readonly')
-		self.prev_button.config(state=NORMAL)
-		self.next_button.config(state=NORMAL)
+		self.prev_button.config(state='normal')
+		self.next_button.config(state='normal')
 
 
 	def disable(self):
 		for widget in self.winfo_children():
-			widget.config(state=DISABLED)
+			widget.config(state='disabled')
 
 
-class LandSwap(Tk):
+class LandSwap(tk.Tk):
 
 	max_decklist_chars = 100
 
 	def __init__(self, *args, **kwargs):
-		Tk.__init__(self, *args, **kwargs)
+		tk.Tk.__init__(self, *args, **kwargs)
 
 		self.resizable(False, False)
 
 		# DECKLIST
 
-		self.decklist_frame = Frame(self)
-		self.decklist_frame.pack(side=LEFT, padx=10, pady=10)
+		self.decklist_frame = tk.Frame(self)
+		self.decklist_frame.pack(side='left', padx=10, pady=10)
 
 		self.decklist_import_button = ttk.Button(self.decklist_frame, text='Import decklist', width=15, command=self.import_decklist)
-		self.decklist_import_button.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+		self.decklist_import_button.grid(row=0, column=0, sticky='w', padx=5, pady=5)
 
 		self.clear_button = ttk.Button(self.decklist_frame, text='Clear', width=7, command=self.clear_text)
-		self.clear_button.grid(row=0, column=0, sticky=E, padx=5, pady=5)
+		self.clear_button.grid(row=0, column=0, sticky='e', padx=5, pady=5)
 
 		self.copy_clipboard_button = ttk.Button(self.decklist_frame, text='Copy to clipboard', width=17, command=self.copy_to_clipboard)
-		self.copy_clipboard_button.grid(row=0, column=1, sticky=E, padx=5, pady=5)
+		self.copy_clipboard_button.grid(row=0, column=1, sticky='e', padx=5, pady=5)
 
-		self.text_box = ModifiedText(self.decklist_frame, relief=FLAT, width=40, height=25, wrap=WORD)
+		self.text_box = ModifiedText(self.decklist_frame, relief='flat', width=40, height=25, wrap='word')
 		self.text_box.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 		self.text_box.bind('<<TextModified>>', self.on_text_modified)
 
-		self.set_state(DISABLED, self.clear_button, self.copy_clipboard_button, self.text_box)
+		self.set_state('disabled', self.clear_button, self.copy_clipboard_button, self.text_box)
 
 		# LANDS
 
@@ -118,7 +118,7 @@ class LandSwap(Tk):
 		self.forest_frame = LandFrame(self, 'Forest')
 
 		for _, frame in self.land_frames.items():
-			frame.pack(side=LEFT)
+			frame.pack(side='left')
 			frame.disable()
 
 
@@ -128,25 +128,25 @@ class LandSwap(Tk):
 			return
 
 		validated = False
-		self.text_box.config(state=NORMAL)
-		self.text_box.insert(END, clipboard)
+		self.text_box.config(state='normal')
+		self.text_box.insert('end', clipboard)
 
 		print('Searching...')
 
 		for land_type in LANDS:
 			for land in LANDS[land_type]:
-				pos = self.text_box.search(land, '1.0', stopindex=END)
+				pos = self.text_box.search(land, '1.0', stopindex='end')
 				if pos:
 					if not validated:
 						validated = True
 					self.text_box.mark_set(land_type, pos)
-					self.text_box.mark_gravity(land_type, LEFT)
+					self.text_box.mark_gravity(land_type, 'left')
 					self.land_frames[land_type].land_var.set(land)
 					self.land_frames[land_type].enable()
 					print('Land found: %s' % land)
 					break
 
-		self.text_box.config(state=DISABLED)
+		self.text_box.config(state='disabled')
 		self.text_box.focus_set()
 
 		if validated:
@@ -156,9 +156,9 @@ class LandSwap(Tk):
 
 
 	def clear_text(self):
-		self.text_box.config(state=NORMAL)
-		self.text_box.delete('1.0', END)
-		self.text_box.config(state=DISABLED)
+		self.text_box.config(state='normal')
+		self.text_box.delete('1.0', 'end')
+		self.text_box.config(state='disabled')
 		self.text_box.focus_set()
 
 		for _, frame in self.land_frames.items():
@@ -183,21 +183,21 @@ class LandSwap(Tk):
 	def on_text_modified(self, event):
 		chars = len(event.widget.get('1.0', 'end-1c'))
 		if chars == 0:
-			self.set_state(DISABLED, self.decklist_import_button, self.clear_button, self.copy_clipboard_button)
-			self.decklist_import_button.config(state=NORMAL)
+			self.set_state('disabled', self.decklist_import_button, self.clear_button, self.copy_clipboard_button)
+			self.decklist_import_button.config(state='normal')
 		else:
-			self.set_state(NORMAL, self.decklist_import_button, self.clear_button, self.copy_clipboard_button)
-			self.decklist_import_button.config(state=DISABLED)
+			self.set_state('normal', self.decklist_import_button, self.clear_button, self.copy_clipboard_button)
+			self.decklist_import_button.config(state='disabled')
 
 
 	def on_land_change(self, land, land_type):
 		frame = self.land_frames[land_type]
 		frame.image_container.config(image=frame.images[land.get()])
 
-		self.text_box.config(state=NORMAL)
+		self.text_box.config(state='normal')
 		self.text_box.delete(land_type, land_type + ' lineend-1c')
 		self.text_box.insert(land_type, land.get())
-		self.text_box.config(state=DISABLED)
+		self.text_box.config(state='disabled')
 
 
 	def set_to_next(self, land_list, land, land_type):
